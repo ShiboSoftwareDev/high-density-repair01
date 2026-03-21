7af48606b2d035c0363b49979ea19f66d5dcd21a
seveibar <seveibar@gmail.com>
2026-03-21 16:40:17 -0700

    wip

---
+++ lib/visualizeHighDensityRepair.ts
@@ -0,0 +1,8 @@
+import type { HighDensityRepair01 } from "./HighDensityRepair01";
+import type { GraphicsObject } from "graphics-debug";
+
+export const visualizeHighDensityRepair = (
+	solver: HighDensityRepair01,
+): GraphicsObject => {
+	// TODO
+};
